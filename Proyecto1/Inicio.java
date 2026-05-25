3b168fca3274fdcbf1e2efab8b88bd35c2e5c7f9
Gabriela Mernies <GabrielaMernies77@hotmail.com>
2026-05-25 20:13:23 -0300

    Add files via upload

--- Proyecto1/Inicio.java
+++ Proyecto1/Inicio.java
@@ -1,3 +1,8 @@
+package inicio;
+
 public class Inicio {
     
+    public static void main(String[] args) {
+        System.out.println("Hola Mundo desde Java!");
+    }
 }
---
+++ Proyecto1/Inicio/Prueba.java
@@ -0,0 +1,7 @@
+package inicio;
+
+public class Prueba {
+public static void main(String[] args) {
+System.out.println("Hola Mundo desde Java!");
+}
+}
